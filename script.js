4b3718f949062405158baddd70b7af02b52883bd
Vinh Pham <vinhhiencute@gmail.com>
2023-06-19 17:37:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Script for navigation bar
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener("click", () => {
+        nav.classList.add("active");
+    });
+}
+
+// Close navigation bar
+const close = document.getElementById("close");
+
+if (close) {
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+}
